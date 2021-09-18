ce97ad43dc9c794a12d94eb1b893f34c110e5d50
Nerdy Rodent <74688049+nerdyrodent@users.noreply.github.com>
2021-09-18 21:13:35 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ pip install -e ./guided-diffusion
 pip install lpips
 
 # Download the diffusion models
+if ! [ -f ./512x512_diffusion_uncond_finetune_008100.pt ]; then
+    curl -COL --http1.1 'https://the-eye.eu/public/AI/models/512x512_diffusion_unconditional_ImageNet/512x512_diffusion_uncond_finetune_008100.pt'  
+fi
 
-curl -OL --http1.1 'https://the-eye.eu/public/AI/models/512x512_diffusion_unconditional_ImageNet/512x512_diffusion_uncond_finetune_008100.pt'
-curl -OL 'https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt'
+if ! [ -f ./256x256_diffusion_uncond.pt ]; then
+    curl -OL 'https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt'
+fi
